197e89dcfcad7c9d32c91b52eee37ec6f783fd0f
Levi Paganucci <levi.paganucci@gmail.com>
2024-05-21 02:57:00 -0300

    Added Things

---
+++ Multiplayer Netcode/Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+
+    private void Awake()
+    {
+        // Adicionando listeners para os eventos OnClick() dos butões
+        // Quando pressionados uma expressão Lambda será executada chamando o método equivalente ...
+        // ... da Singleton NetworkManager
+
+        serverButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
---
+++ Multiplayer Netcode/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    void Update()
+    {
+        // if player is not the owner ob the object, return
+        if (IsOwner) return; // Vem da classe NetworkBehaviour
+        
+        
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W)) moveDirection.z += 1;
+        if (Input.GetKey(KeyCode.S)) moveDirection.z -= 1;
+        if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1;
+        if (Input.GetKey(KeyCode.D)) moveDirection.x += 1;
+
+        moveDirection.Normalize();
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+    }
+}
